10681319fd7a80602688b61cbc8c7710265fde4b
Pavlo Kozlov <kozlov.pavel.s@gmail.com>
2021-06-26 11:55:37 +0200

    Clean up react-app assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import DashboardPage from './pages/dashboard';
 
 function App() {
   return (
     <div className="app">
-      <header className="App-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DashboardPage />
     </div>
   );
 }
---
+++ src/pages/dashboard/DashboardPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from '../../components/Table';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      dashboard page
+      <Table />
+    </div>
+  );
+};
+
+export default DashboardPage;
