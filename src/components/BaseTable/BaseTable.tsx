d41638f474e404ddfa9c5f4b9d9c64c134e3668a
Pavlo Kozlov <kozlov.pavel.s@gmail.com>
2021-06-26 12:48:55 +0200

    Add base table with invoice data

---
+++ src/components/BaseTable/BaseTable.tsx
@@ -0,0 +1,48 @@
+import React, { FC, ReactElement } from 'react';
+import { useTable, Column } from 'react-table';
+import './BaseTable.scss';
+
+interface TableProps {
+  columns: Column[];
+  data: any[];
+}
+
+const Table: FC<TableProps> = ({ columns, data }): ReactElement => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  return (
+    <div className="base-table">
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
-import Table from '../../components/Table';
+import React, { useEffect, useState } from 'react';
+import BaseTable from '../../components/BaseTable';
+import InvoiceDTO from '../../DTO/InvoiceDTO';
+import invoiceService from '../../services/InvoiceService';
 
 const DashboardPage = () => {
+  const [invoices, setInvoices] = useState<InvoiceDTO[]>([]);
+
+  useEffect(() => {
+    invoiceService.getInvoices().then((data) => setInvoices(data));
+  }, []);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'ID',
+        accessor: 'id',
+      },
+      {
+        Header: 'Date',
+        accessor: 'date',
+      },
+      {
+        Header: 'Customer Name',
+        accessor: 'customer_name',
+      },
+      {
+        Header: 'Region',
+        accessor: 'region',
+      },
+      {
+        Header: 'Total',
+        accessor: 'total_invoice',
+      },
+    ],
+    []
+  );
   return (
     <div>
       dashboard page
-      <Table />
+      <BaseTable columns={columns} data={invoices} />
     </div>
   );
 };
